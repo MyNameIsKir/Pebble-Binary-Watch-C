dac4abb29e088a8ec042493739260946224d0248
Kirstin "Kir" Jarchow <kirstinjarchow@gmail.com>
2015-03-28 17:11:09 -0700

    MVP created
    Watch tells time in binary. Will

--- src/main.c
+++ src/main.c
@@ -1,7 +1,39 @@
 #include <pebble.h>
+#include<stdio.h>
 
 Window *s_main_window;
 TextLayer *text_layer;
+char binary_time[27], binary_hours[9], binary_minutes[9], binary_seconds[9];
+
+void getBin(int num, char *str)
+{
+  *(str+5) = '\0';
+  int mask = 0x10 << 1;
+  while(mask >>= 1)
+    *str++ = !!(mask & num) + '0';
+}
+
+static void update_time() {
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+  int hours = tick_time->tm_hour, minutes = tick_time->tm_min, seconds = tick_time->tm_sec;
+
+  if(clock_is_24h_style() == false && hours > 12) {
+      hours = hours - 12;
+  }
+  
+  getBin(hours, binary_hours);
+  getBin(minutes, binary_minutes);
+  getBin(seconds, binary_seconds);
+  
+  strcpy(binary_time, binary_hours);
+  strcat(binary_time, "\n");
+  strcat(binary_time, binary_minutes);
+  strcat(binary_time, "\n");
+  strcat(binary_time, binary_seconds);
+
+  text_layer_set_text(text_layer, binary_time);
+}
 
 static void main_window_load(Window *window) {
   text_layer = text_layer_create(GRect(0, 0, 144, 168));
@@ -16,7 +48,11 @@ static void main_window_load(Window *window) {
 }
 
 static void main_window_unload(Window *window) {
-  
+  text_layer_destroy(text_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void handle_init(void) {
@@ -27,10 +63,11 @@ static void handle_init(void) {
     .unload = main_window_unload
   });
   window_stack_push(s_main_window, true);
+  
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void handle_deinit(void) {
-  text_layer_destroy(text_layer);
   window_destroy(s_main_window);
 }
 
